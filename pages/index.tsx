0a3d1eb700cef1c6b4e68f8d88974850caab0b19
Surasith Kaewvikkit <surasith.kae@ascendcorp.com>
2022-12-04 22:18:44 +0700

    feat: handle redirect and head

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import {
   GetServerSideProps,
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
-  InferGetStaticPropsType,
 } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import router from "next/router";
 import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 type Post = {
   id: number;
   date: Date;
+  guid: DataRendered;
   title: DataRendered;
   content: DataRendered;
   yoast_head_json: YoastHeadJson;
@@ -35,56 +35,75 @@ type OGImage = {
   type: string;
 };
 
-export const getServerSideProps: GetServerSideProps<{ post: Post }> = async (
-  context: GetServerSidePropsContext
-) => {
+const { NEXT_PUBLIC_WP_BASE_URL, NEXT_PUBLIC_WP_TITLE } = process.env;
+
+export const getServerSideProps: GetServerSideProps<{
+  env: any;
+  redirect: boolean;
+  post: Post | null;
+}> = async (context: GetServerSidePropsContext) => {
   const { query } = context;
   const id = query?.p;
+  const redirect = !!query?.redirect;
+  const env = process.env;
+  console.log("redirect:", redirect);
 
-  // if (!id) {
-  //   return {
-  //     props: {},
-  //   };
-  // }
+  if (!id) {
+    return {
+      props: { env, redirect, post: null },
+    };
+  }
 
   const res = await fetch(
-    `https://thaiupdates.info/wp-json/wp/v2/posts?include[]=${id}`
+    `${NEXT_PUBLIC_WP_BASE_URL}/wp-json/wp/v2/posts?include[]=${id}`
   );
   const posts: Post[] = await res.json();
   const post = posts?.[0];
 
   return {
     props: {
+      env,
+      redirect,
       post,
     },
   };
 };
 
 export default function Home({
+  env,
+  redirect,
   post,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   useEffect(() => {
-    console.log("post :", post);
-  }, [post]);
+    if (redirect && post) {
+      router.push(post.guid.rendered);
+    }
+  }, [post, redirect]);
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>{post.title.rendered}</title>
-        <meta name="description" content={post.title.rendered} />
+        <title>{post ? post.title.rendered : env.NEXT_PUBLIC_WP_TITLE}</title>
         <link rel="icon" href="/favicon.ico" />
-        {/* <meta property="og:url" content="your url" /> */}
-        {/* <meta property="og:type" content="website" /> */}
-        {/* <meta property="fb:app_id" content="your fb id" /> */}
-        <meta property="og:title" content={post.yoast_head_json.og_title} />
-        {/* <meta name="twitter:card" content="summary" /> */}
+        <meta property="og:url" content={env.NEXT_PUBLIC_WP_BASE_URL} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content={
+            post ? post?.yoast_head_json.og_title : env.NEXT_PUBLIC_WP_TITLE
+          }
+        />
+        <meta
+          name="description"
+          content={post?.yoast_head_json.og_description}
+        />
         <meta
           property="og:description"
-          content={post.yoast_head_json.og_description}
+          content={post?.yoast_head_json.og_description}
         />
         <meta
           property="og:image"
-          content={post.yoast_head_json.og_image[0].url}
+          content={post?.yoast_head_json.og_image[0].url}
         />
       </Head>
 
